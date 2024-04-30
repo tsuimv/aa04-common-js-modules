93f95bada7f480279e2f61c25be63bb20a65855d
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-04-30 17:13:11 -0500

    finishedp hase 05

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,7 +1,26 @@
 function bindToAnArg(func, arg) {
-  let newFunc = func.bind(arg)
-  return newFunc
+  // let newFunc = func.bind(arg)
+  // return newFunc
+  return func.bind(func, arg)
 }
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const addTwo = bindToAnArg(add, 2);
+const addThree = bindToAnArg(add, 3);
+
+const twoPlusSix = addTwo(6);
+const twoPlusSeven = addTwo(7);
+const threePlusSeven = addThree(7);
+const threePlusEight = addThree(8);
+console.log({
+  twoPlusSix,     // => 8
+  twoPlusSeven,   // => 9
+  threePlusSeven, // => 10
+  threePlusEight  // => 11
+});
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
